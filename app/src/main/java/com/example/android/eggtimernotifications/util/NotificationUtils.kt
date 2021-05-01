709c0421fba35a3d633a04c3a45dbef13e04dd8c
jackjnn <jacknyangenj.jn2@gmail.com>
2021-05-01 13:09:05 +0300

    Added ability of notification to popup

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -103,6 +103,7 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
             )
 
         // TODO: Step 2.5 set priority
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
 
     // TODO: Step 1.4 call notify
     // Deliver the notification
